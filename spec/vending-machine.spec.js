8f2e477e77298de700c9645d4f3d83abf99f9318
Gregory Haskins <greg@greghaskins.com>
2017-03-27 19:24:58 -0400

    accept multiple nickels

--- spec/vending-machine.spec.js
+++ spec/vending-machine.spec.js
@@ -2,20 +2,33 @@ const VendingMachine = require('../lib/vending-machine');
 
 describe('The vending machine', () => {
 
+    const NICKEL = {
+        mass: 5,
+        diameter: 21.21,
+        thickness: 1.95
+    };
+
     it('should display INSERT COIN if no money has been inserted', () => {
         let machine = new VendingMachine();
         expect(machine.nextMessage).toEqual('INSERT COIN');
     });
 
-    it('should accept nickels', () => {
-        let machine = new VendingMachine();
-        machine.insertCoin({
-            mass: 5,
-            diameter: 21.21,
-            thickness: 1.95
+    describe('accepting nickels', () => {
+
+        it('should accept one', () => {
+            let machine = new VendingMachine();
+            machine.insertCoin(NICKEL);
+            expect(machine.nextMessage).toEqual('$0.05');
         });
 
-        expect(machine.nextMessage).toEqual('$0.05');
+        it('should accept multiple', () => {
+            let machine = new VendingMachine();
+            machine.insertCoin(NICKEL);
+            machine.insertCoin(NICKEL);
+            machine.insertCoin(NICKEL);
+            expect(machine.nextMessage).toEqual('$0.15');
+        });
+
     });
 
 
